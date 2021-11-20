2242c24ddfaf57622b4c42d092433da9e1c249c8
monambengouta <monam.bengouta@esprit.tn>
2021-11-20 15:49:27 +0100

    initial commit

---
+++ pass-show-hidden.js
@@ -0,0 +1,11 @@
+const pswrdfield=document.querySelector(".form input[type='password']"),
+      togglebtn=document.querySelector(".form .field i");
+      togglebtn.onclick =()=>{
+          if(pswrdfield.type =="password"){
+              pswrdfield.type ="text";
+              togglebtn.classList.add("active");
+          }else {
+              pswrdfield.type="password";
+              togglebtn.classList.remove("active");
+          }
+      }
